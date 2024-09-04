c40ce13c09a4ef3ae840c6363b2481433b3e91eb
alfaashu <alfaashu@outlook.com>
2024-09-04 20:19:28 +0530

    Children routes implemented using outlet

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Error from './components/Error'
 
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom"
 
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
-            <Body/>
+            <Outlet/>
         </div>
     )
 }
@@ -22,16 +22,23 @@ const appRouter = createBrowserRouter([
     {
     path: "/",
     element: <AppLayout/>,
+    children: [
+        {
+            path: "/",
+            element: <Body/>
+        },
+        {
+            path: "/about",
+            element: <About/>
+        },
+        {
+            path: "/contact",
+            element: <Contact/>
+        }
+    ],
     errorElement: <Error/>
     },
-    {
-    path: "/about",
-    element: <About/>
-    },
-    {
-    path: "/contact",
-    element: <Contact/>
-    }
+    
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
